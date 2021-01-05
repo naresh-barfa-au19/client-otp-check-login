6fbb505542075a0bd363e2725a9a15900eea0576
Naresh Barfa <56158315+nareshbarfa@users.noreply.github.com>
2021-01-05 14:35:02 +0530

    full front end back end
    project login logout  with mobile number otp

---
+++ loginFrontendBackend/client/client-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import signUpCompo from "./components/signup"
+
+function App() {
+  return (
+    <div className="App">
+      <signUpCompo />
+    </div>
+  );
+}
+
+export default App;
